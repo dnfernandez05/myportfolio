08c42265a2bcb2821eba3a884f663a2d7cbb50e7
dnfernandez05 <daphnefernandez000@gmail.com>
2025-10-21 20:26:48 +0800

    final update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
-import Logo from "../../asset/lowgo.svg";
 
 const Sidebar = () => {
   const [toggle, showMenu] = useState(false);
@@ -9,33 +8,45 @@ const Sidebar = () => {
     <>
       <aside className={toggle ? "aside show-menu" : "aside"}>
         <a href="#home" className="nav_logo">
-          <img src={Logo} alt="logo" />
+          <img src="/lowgo.svg" alt="logo" />
         </a>
 
         <nav className="nav">
           <div className="nav__menu">
             <ul className="nav__list">
               <li className="nav__item">
-                <a href="#home" className="nav__link">
+                <a href="#home" className="nav__link" onClick={() => showMenu(false)}>
                   <i className="icon-home"></i>
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#about" className="nav__link">
+                <a href="#about" className="nav__link" onClick={() => showMenu(false)}>
                   <i className="icon-user-following"></i>
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#portfolio" className="nav__link">
+                <a href="#services" className="nav__link" onClick={() => showMenu(false)}>
+                  <i className="icon-briefcase"></i>
+                </a>
+              </li>
+
+              <li className="nav__item">
+                <a href="#resume" className="nav__link" onClick={() => showMenu(false)}>
+                  <i className="icon-graduation"></i>
+                </a>
+              </li>
+
+              <li className="nav__item">
+                <a href="#portfolio" className="nav__link" onClick={() => showMenu(false)}>
                   <i className="icon-layers"></i>
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#contact" className="nav__link">
-                  <i className="icon-phone"></i>
+                <a href="#contact" className="nav__link" onClick={() => showMenu(false)}>
+                  <i className="icon-bubble"></i>
                 </a>
               </li>
             </ul>
@@ -43,15 +54,11 @@ const Sidebar = () => {
         </nav>
 
         <div className="nav__footer">
-          <span className="copyright">&copy; 2025.</span>
+          <span className="copyright">&copy; 2025 </span>
         </div>
       </aside>
 
-      {/* Toggle button */}
-      <div
-        className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"}
-        onClick={() => showMenu(!toggle)}
-      >
+      <div className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"} onClick={() => showMenu(!toggle)}>
         <i className="icon-menu"></i>
       </div>
     </>
